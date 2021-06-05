b2001d492c2b72b93778c129b9b808b28bb57670
Marcos Tulio Pajé <marcostulio06@hotmail.com>
2021-06-05 13:50:08 -0300

    Get Services

---
+++ src/services/GetUserBySocketIdService.ts
@@ -0,0 +1,14 @@
+import { injectable } from "tsyringe";
+import { User } from "../schemas/User";
+
+@injectable()
+class GetUserBySocketId{
+
+  async execute(socket_id:string){
+    const user = await User.findOne({ socket_id })
+
+    return user;
+  }
+}
+
+export { GetUserBySocketId }
--- src/websocket/ChatService.ts
+++ src/websocket/ChatService.ts
@@ -1,8 +1,10 @@
 import { container } from "tsyringe"
 import { io } from "../http"
+import { CreateChatRoomService } from "../services/CreateChatRoomService"
 import { CreateUserService } from "../services/CreateUserService"
 import { GetAllUsersService } from "../services/GetAllUsersService"
-
+import { GetChatRoomByUsersService } from "../services/GetChatRoomByUsersService"
+import { GetUserBySocketId } from "../services/GetUserBySocketIdService"
 io.on("connect", socket =>{
  socket.on("start", async (data) => {
    const {email, avatar, name} = data
@@ -20,4 +22,25 @@ io.on("connect", socket =>{
 
    callback(users)
  })
+
+ socket.on("start_chat", async (data, callback) => {
+  const createChatRoomService = container.resolve(CreateChatRoomService)
+  const getChatRoomByUsersService = container.resolve(GetChatRoomByUsersService)
+  const getUserBySocketIdService = container.resolve(GetUserBySocketId)
+
+  const userLogged = await getUserBySocketIdService.execute(socket.id)
+
+  const users = [data.idUser, userLogged._id]
+
+  let room =  await getChatRoomByUsersService.execute(users)
+
+  if(!room){
+    room =  await createChatRoomService.execute(users)
+  }
+ 
+  callback(room)
+
 })
+
+})
+
